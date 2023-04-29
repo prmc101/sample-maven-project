fbca275993d233a523c2593685dfdc73e2799101
prmc101 <primecommunity101@gmail.com>
2023-04-29 17:02:48 +0100

    Today's Class 29/04/2023 Update

---
+++ src/test/java/com/nopcommerce/demo/testsuite/LoginTest.java
@@ -0,0 +1,35 @@
+package com.nopcommerce.demo.testsuite;
+
+import com.nopcommerce.demo.pages.HomePage;
+import com.nopcommerce.demo.pages.LoginPage;
+import com.nopcommerce.demo.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class LoginTest extends BaseTest {
+
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+
+    @Test
+    public void verifyUserShouldNavigateToLoginPageSuccessfully() {
+        homePage.clickOnLoginLink();
+        String expectedText = "Welcome, Please Sign In!";
+        String actualText = loginPage.getWelcomeText();
+        Assert.assertEquals(actualText, expectedText, "Login page not displayed");
+    }
+
+    @Test
+    public void verifyErrorMessageWithInvalidCredentials() {
+        homePage.clickOnLoginLink();
+        loginPage.enterEmailId("prime123@gmail.com");
+        loginPage.enterPassword("Prime123");
+        loginPage.clickOnLoginButton();
+        String expectedErrorMessage = "Login was unsuccessful. Please correct the errors and try again.\n"
+                + "No customer account found";
+        Assert.assertEquals(loginPage.getErrorMessage(), expectedErrorMessage, "Error message not displayed");
+    }
+}
